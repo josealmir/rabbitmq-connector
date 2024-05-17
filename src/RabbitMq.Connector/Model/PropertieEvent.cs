61f8558de5f9160529788282eb421891507c7df3
Jose Almir <almir2560@gmail.com>
2024-05-17 09:11:20 -0300

    Add properties rabbitMq in header to event

---
+++ src/RabbitMq.Connector/Model/PropertieEvent.cs
@@ -0,0 +1,19 @@
+
+namespace RabbitMq.Connector.Model
+{
+    public enum PropertieEvent
+    {
+        content_type,
+        content_encoding,
+        priority,
+        correlation_id,
+        reply_to,
+        expiration,
+        message_id,
+        timestamp,
+        type,
+        user_id,
+        app_id,
+        cluster_id,
+    }
+}
