9dd0ba86b324ffa048e3bcdbc30ea7bf75c55517
Jose Almir <almir2560@gmail.com>
2024-05-31 14:39:28 -0300

    Add Header and porperty in event

--- src/RabbitMq.Connector/Model/Event.cs
+++ src/RabbitMq.Connector/Model/Event.cs
@@ -1,20 +1,37 @@
-﻿using System.Diagnostics;
+﻿using System.Text.Json.Serialization;
 using MediatR;
 using OperationResult;
 
 namespace RabbitMq.Connector.Model
 {
-    public abstract class Event : IRequest<Result>
+    public abstract class Event :IEventBasicPropertie, IRequest<Result>
     {
         public Event()
-        {                        
-            Name = GetType().Name;
-            Headers.Add(PropertieEvent.correlation_id, Activity.Current?.TraceId.ToString() ?? string.Empty);
-            Headers.Add(PropertieEvent.message_id, Guid.NewGuid().ToString());
-        }
+            => Name = GetType().Name;
 
         public virtual string Name { get; private set; }
-        public virtual DateTime Date { get; private set; } = DateTime.UtcNow;        
-        public IDictionary<PropertieEvent, object> Headers { get; private set; } = new Dictionary<PropertieEvent, object>();
+        public virtual DateTime Date { get; private set; } = DateTime.UtcNow;
+        
+        [JsonIgnore]
+        public IDictionary<BasicPorperties, string> BasicPropertie { get; set; } = new Dictionary<BasicPorperties, string>(12);
+
+        [JsonIgnore]
+        public IDictionary<string, object> Headers { get; set; } = new Dictionary<string, object>(12);
+    }
+
+    public enum BasicPorperties
+    {
+        content_encoding,
+        correlation_id,
+        content_type,
+        message_id,
+        cluster_id,
+        expiration,
+        timestamp,
+        priority,
+        reply_to,
+        user_id,
+        app_id,
+        type,
     }
 }
